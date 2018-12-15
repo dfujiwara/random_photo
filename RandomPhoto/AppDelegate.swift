f015f176dccb42e361cc386b61651c700ca1784e
Daisuke Fujiwara <daisuke.fujiwara@gmail.com>
2018-12-15 14:24:31 -0800

    Initial commit to set up the xcode project

---
+++ RandomPhoto/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  RandomPhoto
+//
+//  Created by Daisuke Fujiwara on 12/15/18.
+//  Copyright © 2018 dfujiwara. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let viewController = ViewController()
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
---
+++ RandomPhoto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomPhoto
+//
+//  Created by Daisuke Fujiwara on 12/15/18.
+//  Copyright © 2018 dfujiwara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.red
+    }
+
+
+}
+
